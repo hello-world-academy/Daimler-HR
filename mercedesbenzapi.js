5c538406f2bbb86b9e9ea8e678dec249dc4e1268
eotp <krois@zedat.fu-berlin.de>
2019-05-18 20:57:03 +0200

    Update mercedesbenzapi.js

--- mercedesbenzapi.js
+++ mercedesbenzapi.js
@@ -3,7 +3,7 @@ function getDealers() {
   
   var city = $('#inputCity').val();
   var url = "https://cors-anywhere.herokuapp.com/https://api.mercedes-benz.com/dealer/v1/dealers?" +
-            "apikey=44782adb-5df3-4024-9b92-ed26a25d0dac" +
+            "apikey=1ff38966-c327-40e8-a3b5-876ab46b6fba" +
             "&pageSize=3" +
             "&countryIsoCode=DE" +
             "&city=" + city;
@@ -13,11 +13,37 @@ function getDealers() {
       addDealer(dealer);
     });
   })
-  .fail(function(jqXHR, textStatus, errorThrown) {
+
+  $.fail(function(jqXHR, textStatus, errorThrown) {
     console.log("error " + textStatus);
     console.log("incoming Text " + jqXHR.responseText);
   }); 
 }
+
+
+function addDealer(dealer) {
+  var contact = dealer["communicationChannels"];
+  console.log(contact);
+  
+  var html = '<div class="card mb-1">' +
+             '<div class="card-body">' +
+             '<h5 class="card-title">' + dealer['legalName'] + '</h5>' +
+             '<p class="card-text"><a href=tel:" ' + contact['phone'] + '"">' + contact['phone'] + '</a></p>' +
+             '<a href="' + contact['website'] + '" target="_blank" class="card-link">' + contact['website'] +'</a>' +
+             '</div>' +
+             '</div>';
+  
+  $("#dealers").append(html);
+}
+
+
+$( document ).ready(function() {
+  console.log("reload");
+  
+  $("#dealerForm").submit(function(event) {
+    event.preventDefault();
+  });
+});
 
 
 function addDealer(dealer) {
